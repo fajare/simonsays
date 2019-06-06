d8bd80bf336f76ba142606cb9aa59ef607bacdb6
Falk Jaresch <fjaresch@gmail.com>
2019-06-06 23:15:03 +0200

    simon-game component

---
+++ src/components/simon-game/simon-game.tsx
@@ -0,0 +1,15 @@
+import '@stencil/router';
+import { Component, h } from '@stencil/core';
+
+@Component({
+    tag: 'simon-game',
+})
+export class SimonGame {
+  render() {
+    return (
+      <stencil-router root="/simonsays/">
+          
+      </stencil-router>
+    );
+  }
+}
